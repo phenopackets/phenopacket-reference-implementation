1fc5e7791d37ab87636aacff021a3003750ed701
Chris Mungall <cjm@berkeleybop.org>
2016-03-10 22:16:30 -0800

    Added additional test for assocations

--- src/test/java/org/monarchinitiative/ppk/model/condition/PhenotypeAssociationTest.java
+++ src/test/java/org/monarchinitiative/ppk/model/condition/PhenotypeAssociationTest.java
@@ -1,10 +1,16 @@
 package org.monarchinitiative.ppk.model.condition;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
+import org.monarchinitiative.ppk.PhenoPacket;
 import org.monarchinitiative.ppk.io.JsonGenerator;
 import org.monarchinitiative.ppk.io.YamlGenerator;
+import org.monarchinitiative.ppk.io.YamlReader;
+import org.monarchinitiative.ppk.model.organism.Person;
 
 import java.io.IOException;
+import java.util.List;
 
 public class PhenotypeAssociationTest {
 
@@ -25,5 +31,24 @@ public class PhenotypeAssociationTest {
 		System.out.println(YamlGenerator.render(pa));
 
 	}
+	
+	/**
+	 * TODO: this isn't working...
+	 * 
+	 * @throws IOException
+	 */
+	//@Test
+	public void testFromFile() throws IOException {
+		ClassLoader classLoader = getClass().getClassLoader();
+		PhenoPacket packet = YamlReader.readFile(classLoader.getResource("condition/person-phenotype-example1.yaml").getFile());
+		System.out.println(YamlGenerator.render(packet));
+		List<Person> persons = packet.getPersons();
+		assertTrue(persons.size() == 3);
+		// order is preserved
+		assertTrue(persons.get(0).getSex().equals("M"));
+		System.out.println(persons.get(0).getId());
+		//assertEquals("#1", persons.get(0).getId());  TODO - FIXME
+	}
+
 
 }
